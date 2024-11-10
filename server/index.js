92268e549bd516ca2c8bb6eb398377b2ea8c5b91
ishmaam-ik <ishmaam@iut-dhaka.edu>
2024-11-10 14:29:24 +0600

    Learning helmet

--- server/index.js
+++ server/index.js
@@ -9,11 +9,35 @@ import multer from "multer";
 import { fileURLToPath } from "url";
 import path from "path";
 
-const __fileName= fileURLToPath(import.meta.url);   //gets the file name of current file
-const __dirName= path.dirname(__fileName);  //
+const __fileName= fileURLToPath(import.meta.url);  
+const __dirName= path.dirname(__fileName);
 const app= express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        useDefaults: false, 
+        directives:{
+            defaultSrc: ["'self'"], 
+            scriptSrc: ["'self'", "'www.example.com'"],
+            objectSrc: ["'none'"], 
+            upgradeInsecureRequests: [], 
+        },
+    },
+    crossOriginOpenerPolicy: {
+        policy: 'same-origin-allow-popups'
+    },
+    crossOriginResourcePolicy: {
+        policy: 'same-origin'
+    }, 
+    referrerPolicy: {
+        policy: ['origin', 'no-referrer-when-downgrade', 'strict-origin-when-cross-origin']
+    },
+    strictTransportSecurity:{
+        maxAge: 15552000   
+    }
+}))
 
 console.log(__fileName);
 console.log(__dirName);
