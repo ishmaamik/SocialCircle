aefc722081c3565124394e9a0fd1f3268c939634
ishmaam-ik <ishmaam@iut-dhaka.edu>
2024-11-22 22:01:55 +0600

    Started Multer upload

--- server/index.js
+++ server/index.js
@@ -12,13 +12,39 @@ import path from "path";
 const __fileName= fileURLToPath(import.meta.url);  
 const __dirName= path.dirname(__fileName);
 const app= express();
+const PORT= 8000;
+
+const storage= multer.diskStorage({
+    destination: function(req, file, cb){
+        return cb(null, './uploads');    //null means no error handling and destination upload to ./uploads
+    },
+
+    filename: function(req, file, cb){
+        return cb(null, `${Date.now()}-${file.originalname}`);    //returns the unique filename which has appended to date time
+    }
+})
+
+const upload= multer({storage: storage});
+
 
 app.use(helmetConfig);  //don't use helmetConfig() rather helmetConfig only since helmet() is already configured and initialized in helmet.js.
 app.use(morgan("common"));
 app.use(express.json({limit:"30mb", extended: true}));
 app.use(express.urlencoded({limit:"30mb", extended: true}));    //bodyParser is deprecated middleware
 app.use(cors());
+app.listen(PORT, ()=>console.log(`App listening on ${PORT}`));
 
+app.post('/uploads', upload.single('profileImage'), (req, res)=>{
+        console.log(req.body);
+        console.log(req.file);
+
+        return res.redirect("/");
+})
+
+app.use('/',(req, res)=>{
+    const htmlFile= path.join(__dirName, 'frontend.html');
+    res.sendFile(htmlFile);
+})
 console.log(__fileName);
 console.log(__dirName);
 
