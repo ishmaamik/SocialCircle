7dd7b287ccbd5e60a47cd88d9dc0615e61ecadea
ishmaam-ik <ishmaam@iut-dhaka.edu>
2024-12-03 14:55:34 +0600

    User routes done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,4 @@
-import User from "../model/User";
+import User from "../model/User.js";
 
 export const getUser=async(req, res)=>{
     try{
@@ -21,18 +21,58 @@ export const getUser=async(req, res)=>{
 
 export const getUserFriends=async(req, res)=>{
     try{
-        const{username}= req.body;
-        const user= await User.findOne({username: username});
+        const{userName}= req.body;
+        const user= await User.findOne({username: userName});
 
         if(!user){
             res.status(404).json("User does not exist");
         }
 
         const userFriends= await Promise.all(
-            user.friends.map((username)=>User.findOne({username}))  //extracts to the username variable the usernames
+            user.friends.map((userName)=>User.findOne({userName}))  //extracts to the username variable the usernames
         )
+        
+        const formattedFriends= userFriends.map(
+            ({userName, firstName, lastName, occupation, location, picturePath})=>{
+                return{userName, firstName, lastName, occupation, location, picturePath}
+            }
+        )
+        res.status(200).json(formattedFriends);
+    }
+    catch(error){
+        res.status(500).json(error.message);
+    }
+}
 
-        res.status(200).json(userFriends);
+export const addRemoveFriend= async(req, res)=>{
+    try{
+        const{userName, friendUsername}= req.body;
+        const user= await User.findOne({username: userName});
+        const friend= await User.findOne({username: friendUsername});
+
+        if(user.friends.includes(friendUsername)){
+            user.friends= user.friends.filter((username)=> username!== friendUsername);
+            friend.friends= friend.friends.filter((username)=> username!==userName);
+        }
+        else{
+            user.friends.push(friendUsername);
+            friend.friends.push(userName);
+        }
+
+        await user.save();
+        await friend.save();
+
+        const userFriends= await Promise.all(
+            user.friends.map((userName)=>User.findOne({userName}))  //extracts to the username variable the usernames
+        )
+        
+        const formattedFriends= userFriends.map(
+            ({userName, firstName, lastName, occupation, location, picturePath})=>{
+                return{userName, firstName, lastName, occupation, location, picturePath}
+            }
+        )
+        res.status(200).json(formattedFriends);
+
     }
     catch(error){
         res.status(500).json(error.message);
--- server/index.js
+++ server/index.js
@@ -1,27 +1,19 @@
 import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import morgan from "morgan";
 import helmetConfig from "./middlewares/helmet.js";
-import dotenv from "dotenv";
 import { upload } from "./middlewares/multer.js";
 import { fileURLToPath } from "url";
 import path from "path";
-import authRoutes from "./routes/auth.js"
+import authRoutes from "./routes/authRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import { Connection } from "./middlewares/connect.js";
 
-
-dotenv.config();    //A must NEED
-
-const username= process.env.DB_USERNAME;
-const password= process.env.DB_PASSWORD;
 const __fileName= fileURLToPath(import.meta.url);  
 const __dirName= path.dirname(__fileName);
 const app= express();
 const PORT= 8000;
-const URL = `mongodb+srv://${username}:${password}@cluster0.zc15p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
-
 
 app.use(helmetConfig);  //don't use helmetConfig() rather helmetConfig only since helmet() is already configured and initialized in helmet.js.
 app.use(morgan("common"));
@@ -37,26 +29,12 @@ app.post('/uploads', upload.single('profileImage'), (req, res)=>{
         return res.redirect("/");
 })
 
-const connection=async()=>{
-    try {
-        await mongoose.connect(URL);
-        console.log('Database Connected Successfully');
-    } catch(error) {
-        console.log('Error: ', error.message);
-    }
-};
+ Connection();
 
- connection();
  app.use("/auth", authRoutes);  //order of "/" and "/auth" matters!
+ app.use("/user", userRoutes);
 
  app.use("/",(req, res)=>{
     const htmlFile= path.join(__dirName, 'frontend.html');
     res.sendFile(htmlFile);
 })
-
-
-
-
-console.log(__fileName);
-console.log(__dirName);
-
